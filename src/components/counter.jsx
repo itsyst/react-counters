89b2767013060ca2508ff653add945f5a1db2a51
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 01:16:40 +0200

    Create counter component.

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello World</h1>
+            </div>
+        )
+    }
+}
