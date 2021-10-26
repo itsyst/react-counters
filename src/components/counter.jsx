a1105e4c5ddffb0f38ac72e3d90be0f1c5649632
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 16:18:28 +0200

    Render class dynamically.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,34 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 export default class Counter extends Component {
-    state = {
-        count: 0,
-    };
-    styles = {
-        fontSize: 50,
-        fontWeight: "bold",
-    };
+	state = {
+		count: 0
+	};
+	// styles = {
+	//     fontSize: 50,
+	//     fontWeight: "bold",
+	// };
 	render() {
 		return (
 			<>
-				<span style ={{fontSize:30}} className="badge bg-dark m-2">
+				<span
+					/*style ={{fontSize:30}}*/ className={this.getBadgeClasses()}
+				>
 					{this.formatCount()}
 				</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 			</>
-		)
+		);
+	}
+
+	getBadgeClasses() {
+		let classes = 'badge m-2 rounded-pill bg-';
+		classes += this.state.count === 0 ? 'warning' : 'dark';
+		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state
-		return count === 0 ? "Zero" : count
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count;
 	}
 }
