9b02dd44e9cff82cdbae7dfda54120cb58ce3afe
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 12:56:33 +0200

    Embde expression.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
+    state = {
+        count: 0
+    };
     render() {
         return (
             <>
-                <h1>hello World</h1>
+                <span>{this.formatCount() }</span>
                 <button type="button" class="btn btn-primary">Increment</button>
             </>
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return (count !== 0)? count: <h1>Zero</h1> 
     }
 }
