323f8d2eed250aa679a614ddd4efe136ade5ee6f
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 12:46:16 +0200

    use react fragment.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export default class Counter extends Component {
     render() {
         return (
-            <div>
+            <>
                 <h1>hello World</h1>
                 <button type="button" class="btn btn-primary">Increment</button>
-            </div>
+            </>
         );
     }
 }
