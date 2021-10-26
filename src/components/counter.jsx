179da4dddc0daa52187141ee3965adec9bfb3002
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 16:36:02 +0200

    Render lists dynamicaly.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
 	state = {
-		count: 0
+		count: 0,
+		tags: [
+			{ id: 1, name: 'tag1' },
+			{ id: 2, name: 'tag2' },
+			{ id: 3, name: 'tag3' }
+		]
 	};
 	// styles = {
 	//     fontSize: 50,
@@ -17,13 +22,18 @@ export default class Counter extends Component {
 					{this.formatCount()}
 				</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
+				<ul>
+					{this.state.tags.map((tag) => (
+						<li key={tag.id}>{tag.name}</li>
+					))}
+				</ul>
 			</>
 		);
 	}
 
 	getBadgeClasses() {
 		let classes = 'badge m-2 rounded-pill bg-';
-		classes += this.state.count === 0 ? 'warning' : 'dark';
+		classes += this.state.count === 0 ? 'warning' : 'primary';
 		return classes;
 	}
 
