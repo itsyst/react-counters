31437cd169fdf113e77e1cdd6b114e50048fa2e9
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 16:46:27 +0200

    Conditional rendering.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,15 +4,19 @@ export default class Counter extends Component {
 	state = {
 		count: 0,
 		tags: [
-			{ id: 1, name: 'tag1' },
-			{ id: 2, name: 'tag2' },
-			{ id: 3, name: 'tag3' }
+			// { id: 1, name: 'tag1' },
+			// { id: 2, name: 'tag2' },
+			// { id: 3, name: 'tag3' }
 		]
 	};
 	// styles = {
 	//     fontSize: 50,
 	//     fontWeight: "bold",
 	// };
+	renderTags() {
+		if (this.state.tags.length === 0) return <h1>There are no tags!</h1>;
+		return this.state.tags.map((tag) => <li key={tag.id}>{tag.name}</li>);
+	}
 	render() {
 		return (
 			<>
@@ -23,9 +27,8 @@ export default class Counter extends Component {
 				</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 				<ul>
-					{this.state.tags.map((tag) => (
-						<li key={tag.id}>{tag.name}</li>
-					))}
+					{this.state.tags.length === 0 && 'There are no tags!'}
+					{this.renderTags()}
 				</ul>
 			</>
 		);
