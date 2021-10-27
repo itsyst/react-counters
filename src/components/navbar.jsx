394fc292a4ed63db0d24ba9dd099d3c839bce88a
Khaled El Hamzi <contact@elhamzi.me>
2021-10-28 00:46:19 +0200

    Staeless functional component.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Navbar extends Component {
-	render() {
-		console.log(this.props);
-		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<div className="container">
-					<span className="navbar-brand mb-0 h1">
-						Counters
-						<span className="badge rounded-pill bg-secondary  mx-3 ">
-							{this.props.totalCounters}
-						</span>
+// Stateless functional component
+const Navbar = (props) => {
+	return (
+		<nav className="navbar navbar-light bg-light mb-3">
+			<div className="container">
+				<span className="navbar-brand mb-0 h1">
+					Counters
+					<span className="badge rounded-pill bg-secondary  mx-3 ">
+						{props.totalCounters}
 					</span>
-				</div>
-			</nav>
-		);
-	}
-}
+				</span>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
